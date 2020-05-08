d081d25cfe0d146a7a2082b0d8e2c3c3f51943a0
Zyknafein <deneux.j@gmail.com>
2020-05-08 15:53:59 +0200

    Refactor

---
+++ myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
@@ -0,0 +1,72 @@
+package com.dummy.myerp.consumer.dao.impl.db.dao;
+
+import com.dummy.myerp.consumer.db.AbstractDbConsumer;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+public class ComptabiliteDaoImplTest extends AbstractDbConsumer {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getListCompteComptable() {
+    }
+
+    @Test
+    void getListJournalComptable() {
+    }
+
+    @Test
+    void getListEcritureComptable() {
+    }
+
+    @Test
+    void getEcritureComptable() {
+    }
+
+    @Test
+    void getEcritureComptableByRef() {
+    }
+
+    @Test
+    void loadListLigneEcriture() {
+    }
+
+    @Test
+    void insertEcritureComptable() {
+    }
+
+    @Test
+    void insertListLigneEcritureComptable() {
+    }
+
+    @Test
+    void updateEcritureComptable() {
+    }
+
+    @Test
+    void deleteEcritureComptable() {
+    }
+
+    @Test
+    void deleteListLigneEcritureComptable() {
+    }
+
+    @Test
+    void getSequenceEcritureComptable() {
+    }
+
+    @Test
+    void insertSequenceEcritureComptable() {
+    }
+
+    @Test
+    void updateSequenceEcritureComptable() {
+    }
+}

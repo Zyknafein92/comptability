ee2358498bbf719f5d907a622601eedd9db6b576
Zyknafein <deneux.j@gmail.com>
2020-05-15 17:55:21 +0200

    Add some test in consumer, try to build with travis.

--- myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
+++ myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
@@ -1,37 +1,61 @@
 package com.dummy.myerp.consumer.dao.impl.db.dao;
 
+import com.dummy.myerp.consumer.dao.contrat.ComptabiliteDao;
 import com.dummy.myerp.consumer.db.AbstractDbConsumer;
+import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
+import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.technical.exception.NotFoundException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+
+@SpringJUnitConfig(locations = {
+        "classpath:bootstrapContext.xml"})
 
 public class ComptabiliteDaoImplTest extends AbstractDbConsumer {
 
+    @Autowired
+    ComptabiliteDao comptabiliteDao;
+
+    /* ListCompteComptable */
+
     @Test
-    void getInstance() {
+    void testGetListCompteComptableShouldReturnListNotEmpty() {
+        assertThat(comptabiliteDao.getListCompteComptable().isEmpty()).isFalse();
     }
 
     @Test
-    void getListCompteComptable() {
+    void testGetListJournalComptableShouldReturnListNotEmpty() {
+        List<JournalComptable> listJournal = comptabiliteDao.getListJournalComptable();
+
+        assertFalse(listJournal.isEmpty());
     }
 
     @Test
-    void getListJournalComptable() {
+    void testGetListEcritureComptableShouldReturnListNotEmpty() {
+        List<EcritureComptable> listEcritureComptable = comptabiliteDao.getListEcritureComptable();
+
+        assertFalse(listEcritureComptable.isEmpty());
     }
 
     @Test
-    void getListEcritureComptable() {
+    void testGetEcritureComptableGivenIDReturnNotNull() throws NotFoundException {
+        EcritureComptable ecritureComptable = comptabiliteDao.getEcritureComptable(-2);
+        assertNotNull(ecritureComptable);
     }
 
     @Test
-    void getEcritureComptable() {
-    }
+    void testGetEcritureComptableByRefReturnEcritureComptable() throws NotFoundException {
+        EcritureComptable ecritureComptable = comptabiliteDao.getEcritureComptableByRef("AC-2016/00001");
 
-    @Test
-    void getEcritureComptableByRef() {
+        assertNotNull(ecritureComptable);
     }
 
     @Test
